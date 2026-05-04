1abc5c169733b1af6f193de6a27d36bf5b44ee6a
Yusup Hambali <sup.alpuket@gmail.com>
2026-05-05 03:40:05 +0700

    Fix isEscaped function to check index bounds

--- programming.zig
+++ programming.zig
@@ -22,7 +22,7 @@ pub fn readStringDelimiter(parser: anytype) !u8 {
 
 /// Cek apakah karakter saat ini adalah escape backslash (\)
 pub fn isEscaped(parser: anytype) bool {
-    return parser.index < parser.source.len and parser.source[parser.index] == '\\';
+    return parser.source[parser.index] == '\\';
 }
 
 pub fn needString(parser: anytype) ![]const u8 {
@@ -31,7 +31,8 @@ pub fn needString(parser: anytype) ![]const u8 {
 
     while (parser.index < parser.source.len) {
         if (isEscaped(parser)) {
-            parser.index += 2; // skip \ dan char berikutnya (termasuk \")
+   if (parser.index + 1 >= parser.source.len) return error.UnterminatedString;
+parser.index += 2; // skip \ dan char berikutnya (termasuk \")
             continue;
         }
         if (parser.source[parser.index] == quote) break;
